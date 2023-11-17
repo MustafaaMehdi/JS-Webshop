97c050bf4fd78833d0de7d38a2c04cbc100e0a0b
MustafaaMehdi <146783408+MustafaaMehdi@users.noreply.github.com>
2023-11-17 22:15:21 +0100

    :art: Styke to navbar and intro section

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// HEADER:
+// {
+
+    //1 Burge Menu:
+    // - Create button for the burger menu 
+    // - Style button accordingly 
+    // - Create a requirement for an if menu is visible
+    // - create a button for expanded Burger menu
+    // - Create function to make the button able to Toggle the navigation menu on and off
+
+    // 1.1 Add Dark Theme/Light Theme functionality button:
+    // - Once the burger menu is expanded /create a button in CSS/
+    // - customer should be able to toggle theme via button
+
+    //2 Navigation meny sticky on scroll up:
+    // - on scroll down the navigation should collapse
+    // - When user is scrolling upp, the navigation should expand
+
+    // 3a Click on button to show order summary
+    // - Summary should be toggled
+    // - When clicked a summary should be animated (css class)
+    // - Create a toggle for the visually hidden class 
+    // - Add products variable to the summary
+    // - Add totalprice variable to the summary
+    // - Add product removal button to the summary 
+
+
+    // 3b Update shopping card visuals on product addition:
+    // - Per default, shopping card should be stock navBar color 
+    // - As customers adds a product, the shopping card should change color and expand 
+    // - TotalPrice value should be added upon a customer adding product  
+    // - For each product that is added, some kind of visual feedback is presented and TotalPrice value is updated
+
+// }
+
