5058594f930880e39dc51e5eacf5b4c597c36c33
MustafaaMehdi <146783408+MustafaaMehdi@users.noreply.github.com>
2023-11-21 00:36:48 +0100

    :art: Style to checkout/order summary

--- script.js
+++ script.js
@@ -3,40 +3,40 @@
 // {
 
     //1 Burge Menu:
-    // - Create button for the burger menu 
-    // - Style button accordingly 
-    // - Create a requirement for an if menu is visible
-    // - create a button for expanded Burger menu
-    // - Create function to make the button able to Toggle the navigation menu on and off
+    // [] Create button for the burger menu 
+    // [] Style button accordingly 
+    // [] Create a requirement for an if menu is visible
+    // [] create a button for expanded Burger menu
+    // [] Create function to make the button able to Toggle the navigation menu on and off
 
     // 1.1 Burge Menu navigation:
-    // - When clicking on a menu option/Section menu should toggle and scroll to section
+    // [] When clicking on a menu option/Section menu should toggle and scroll to section
     
 
     // 1.2 Add Dark Theme/Light Theme functionality button:
-    // - Once the burger menu is expanded /create a button in CSS/
-    // - customer should be able to toggle theme via button
+    // [] Once the burger menu is expanded /create a button in CSS/
+    // [] customer should be able to toggle theme via button in navigation
 
     //2 Navigation meny sticky on scroll up:
-    // - on scroll down the navigation should collapse
-    // - When user is scrolling upp, the navigation should expand
+    // [] on scroll down the navigation should collapse
+    // [] When user is scrolling upp, the navigation should expand
 
     // 3.a Click on cart icon in the menu to show order preview
-    // - Summary should be toggled
-    // - When clicked a summary should be animated (css class)
-    // - Create a toggle for the visually hidden class 
-    // - Show orders as individual articles in summary 
-    // - add possibility to remove individual items from the preview
-    // - Add products variable to the summary
-    // - Add totalprice variable to the summary
-    // - Add product removal button to the summary 
+    // [] Summary should be toggled
+    // [] When clicked a summary should be animated (css class)
+    // [] Create a toggle for the visually hidden class 
+    // [] Show orders as individual articles in summary 
+    // [] add possibility to remove individual items from the preview
+    // [] Add products variable to the summary
+    // [] Add totalprice variable to the summary
+    // [] Add product removal button to the summary 
 
 
     // 3.b Update shopping cart visuals on product addition:
-    // - Per default, shopping card should be stock navBar color 
-    // - As customers adds a product, the shopping card should change color and expand 
-    // - TotalPrice value should be added upon a customer adding product  
-    // - For each product that is added, some kind of visual feedback is presented and TotalPrice value is updated
+    // [] Per default, shopping card should be stock navBar color 
+    // [] As customers adds a product, the shopping card should change color and expand 
+    // [] TotalPrice value should be added upon a customer adding product  
+    // [] For each product that is added, some kind of visual feedback is presented and TotalPrice value is updated
 
 // }
 
@@ -48,21 +48,29 @@
 
     // PRODUCTS PAGE
         //1.a Add Products with a for loop:
-        // - Create a variable for the products container
-        // - create an Array containing all objects 
-        // - Select the productsContainer 
-        // - Push the Image, Title and Price inside of an <article> with a class
+        // [] Create a variable for the products container
+        // [] create an Array containing all objects 
+        // [] Select the productsContainer 
+        // [] Push the Image, Title and Price inside of an <article> with a class
 
         //1.b Add Products buttons with a for loop:
-        // - Select the productsContainer 
-        // - Push the subrtract and add buttons
+        // [] Select the productsContainer 
+        // [] Push the subrtract and add buttons
+
+        //1.b Add Products buttons with a for loop:
+        // [] Select the productsContainer 
+        // [] Push the subrtract and add buttons
 
 
-    // REVIEWS
-        //1 Add reviews with a for loop:
-        // - Create a variable for the reviews container
-        // - create an Array with the 
-        // - Select the reviewsContainer 
-        // - Push the Image, H3 and P inside of an <article> with a class
+        // CHECKOUT PAGE
+        // 1.a Remove products:
+        // 1.b Add/subtract products in cart:
+        // 2.a Validation of forms
+        // 2.b Toggle between invoice and card
+        // 3.a Newsletter checked in per default
+        // 3.b submit button to complete order grayed out until information filled in/validated
+
+
+
 
 // }
