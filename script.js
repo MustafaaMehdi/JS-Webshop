eaecc7c176ed4d0f2a329ec97fc24559babab377
MustafaaMehdi <146783408+MustafaaMehdi@users.noreply.github.com>
2023-11-26 15:16:16 +0100

    ✨ Add/Remove feature from cart and summary

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //HEADER: VARIABLE to select the BURGER MENU open button
 const burgerMenuBtn = document.querySelector('.OpenNavBtn');
 
@@ -18,7 +17,6 @@ const closeNavBtn = document.querySelector('.closeNavBtn');
 closeNavBtn.addEventListener('click', burgerMenuClose);
 //define FUNCTION to CLOSE BURGER MENU
 function burgerMenuClose() {
-
 	console.log(openBurgeMenu);
 	openBurgeMenu.classList.add('toggleHide');
 }
@@ -52,7 +50,20 @@ function cartClose() {
 	console.log(openBurgeMenu);
 	cartPreview.classList.add('toggleHide');
 }
+//Variabel for the proceed to checkout BTN
+const checkoutBtn = document.querySelector('.checkoutBtn');
+checkoutBtn.addEventListener('click', orderSummary);
 
+//Function for the proceed to checkout BTN
+function orderSummary() {
+	const checkOutPage = document.querySelector('.checkoutWrapper');
+	checkOutPage.classList.remove('toggleHide');
+
+	const mainPageWrapper = document.querySelector('.mainSectionWrapper');
+	mainPageWrapper.classList.add('toggleHide');
+
+	cartPreview.classList.add('toggleHide');
+}
 // PRODUCT SECTION:
 // {
 //2 SORTING:
@@ -70,7 +81,6 @@ function sortToggle() {
 	sortOptions.classList.toggle('toggleHide');
 }
 
-
 // PRODUCTS PAGE
 //1.a Add Products with a for loop:
 // [x] Create a variable for the products container
@@ -82,6 +92,7 @@ const productStock = [
 	{
 		name: "Segreto D'Amore",
 		Amount: 0,
+		serialNo: 1,
 		price: 28.99,
 		category: { shape: 'heart', color: 'red' },
 		rating: 4.8,
@@ -95,6 +106,7 @@ const productStock = [
 	{
 		name: 'Cesto Di Rose',
 		Amount: 0,
+		serialNo: 2,
 		price: 34.99,
 		category: { shape: 'round', color: 'red' },
 		rating: 3.9,
@@ -108,6 +120,7 @@ const productStock = [
 	{
 		name: 'Gioiello Di Fiori',
 		Amount: 0,
+		serialNo: 3,
 		price: 29.99,
 		category: { shape: 'round', color: 'blue' },
 		rating: 2.9,
@@ -121,6 +134,7 @@ const productStock = [
 	{
 		name: 'Cuore Infinito',
 		Amount: 0,
+		serialNo: 4,
 		price: 49.99,
 		category: { shape: 'square', color: 'transparent' },
 		rating: 4.9,
@@ -134,6 +148,7 @@ const productStock = [
 	{
 		name: 'Dolce Rose',
 		Amount: 0,
+		serialNo: 5,
 		price: 29.99,
 		category: { shape: 'round', color: 'white' },
 		rating: 4.9,
@@ -147,6 +162,7 @@ const productStock = [
 	{
 		name: 'Notte Romantica',
 		Amount: 0,
+		serialNo: 6,
 		price: 49.99,
 		category: { shape: 'square', color: 'black' },
 		rating: 4.9,
@@ -160,6 +176,7 @@ const productStock = [
 	{
 		name: 'Gioiello Di Rose Bianche',
 		Amount: 0,
+		serialNo: 7,
 		price: 34.99,
 		category: { shape: 'square', color: 'white' },
 		rating: 4.3,
@@ -173,6 +190,7 @@ const productStock = [
 	{
 		name: 'Cuore di Rose Blu',
 		Amount: 0,
+		serialNo: 8,
 		price: 19.99,
 		category: { shape: 'round', color: 'white' },
 		rating: 3.3,
@@ -186,6 +204,7 @@ const productStock = [
 	{
 		name: 'Amore Eterno',
 		Amount: 0,
+		serialNo: 9,
 		price: 39.99,
 		category: { shape: 'heart', color: 'black' },
 		rating: 4.1,
@@ -199,6 +218,7 @@ const productStock = [
 	{
 		name: 'Dolcezza In Rosa',
 		Amount: 0,
+		serialNo: 10,
 		price: 19.99,
 		category: { shape: 'round', color: 'pink' },
 		rating: 3.1,
@@ -212,6 +232,7 @@ const productStock = [
 	{
 		name: 'Gioia Del Cuore',
 		Amount: 0,
+		serialNo: 11,
 		price: 34.99,
 		category: { shape: 'heart', color: 'white' },
 		rating: 4.4,
@@ -225,6 +246,7 @@ const productStock = [
 	{
 		name: 'Serenata Rosata',
 		Amount: 0,
+		serialNo: 12,
 		price: 24.99,
 		category: { shape: 'round', color: 'pink' },
 		rating: 3.7,
@@ -238,6 +260,7 @@ const productStock = [
 	{
 		name: 'Dolce Amore',
 		Amount: 0,
+		serialNo: 13,
 		price: 29.99,
 		category: { shape: 'round', color: 'white' },
 		rating: 4.5,
@@ -253,6 +276,7 @@ const productStock = [
 	{
 		name: 'Dolce Abbraccio',
 		Amount: 0,
+		serialNo: 14,
 		price: 29.99,
 		category: { shape: 'bear', color: 'white' },
 		rating: 4.6,
@@ -266,6 +290,7 @@ const productStock = [
 	{
 		name: 'Coccole Di Noce',
 		Amount: 0,
+		serialNo: 15,
 		price: 21.99,
 		category: { shape: 'bear', color: 'brown' },
 		rating: 3.6,
@@ -279,6 +304,7 @@ const productStock = [
 	{
 		name: 'Gioiello Del Cuore',
 		Amount: 0,
+		serialNo: 16,
 		price: 21.99,
 		category: { shape: 'bear', color: 'grey' },
 		rating: 4.5,
@@ -292,6 +318,7 @@ const productStock = [
 	{
 		name: "Orsetto D'Amore",
 		Amount: 0,
+		serialNo: 17,
 		price: 29.99,
 		category: { shape: 'bear', color: 'red' },
 		rating: 4.8,
@@ -305,6 +332,7 @@ const productStock = [
 	{
 		name: 'Rosa Incanto',
 		Amount: 0,
+		serialNo: 18,
 		price: 25.99,
 		category: { shape: 'bear', color: 'pink' },
 		rating: 4.4,
@@ -318,6 +346,7 @@ const productStock = [
 	{
 		name: 'Sogno Azzurro',
 		Amount: 0,
+		serialNo: 19,
 		price: 14.99,
 		category: { shape: 'bear', color: 'blue' },
 		rating: 4.7,
@@ -330,14 +359,11 @@ const productStock = [
 	},
 ];
 
-
 // [X] Select the product container from the HTML DOM
 // [Used Class since it's unique]   Add ID to the container in HTML ()
 const productContainer = document.querySelector('.productContainer');
 //Test to see if correct Element is selected
 // console.log(productContainer);
-
-
 
 // [X] Push the Image, Title and Price inside of an <article> with a class
 
@@ -346,9 +372,9 @@ const productContainer = document.querySelector('.productContainer');
 // [X] Push template for articles as innerHTML
 // [X] Push the subrtract and add buttons
 function pushProductStock() {
-	productContainer.innerHTML = "";
-for (let i = 0; i < productStock.length; i++) {
-	productContainer.innerHTML += `<article class="productArticles">
+	productContainer.innerHTML = '';
+	for (let i = 0; i < productStock.length; i++) {
+		productContainer.innerHTML += `<article class="productArticles">
 	<h3 class="articleTitle">${productStock[i].name}</h3>
 	<img
 		class="articleImage"
@@ -370,58 +396,252 @@ for (let i = 0; i < productStock.length; i++) {
 </div>
 
 
-</article>`
-
-  }
-//Define a variable for the ADD TO CART
-const addToCartBtn = document.querySelectorAll('.articleAddToCart');
-//Create a loop to add click event for the ADD TO CART btns
-for (let i = 0; i < addToCartBtn.length; i++) {
-	addToCartBtn[i].addEventListener('click', addToCart)
+</article>`;
+	}
+	//Define a variable for the ADD TO CART
+	const addToCartBtn = document.querySelectorAll('.articleAddToCart');
+	//Create a loop to add click event for the ADD TO CART btns
+	for (let i = 0; i < addToCartBtn.length; i++) {
+		addToCartBtn[i].addEventListener('click', addToCart);
+	}
+	//Define a variable for the REMOVE FROM CART btn
+	const subtractFromCartBtn = document.querySelectorAll(
+		'.articleRemoveFromCart'
+	);
+	// //Create a loop to add click event for the ADD TO CART btns
+	for (let i = 0; i < subtractFromCartBtn.length; i++) {
+		subtractFromCartBtn[i].addEventListener('click', subtractFromCart);
+	}
 }
-//Define a variable for the REMOVE FROM CART btn
-const removeFromCartBtn = document.querySelectorAll('.articleRemoveFromCart');
-//Create a loop to add click event for the ADD TO CART btns
-for (let i = 0; i < removeFromCartBtn.length; i++) {
-	removeFromCartBtn[i].addEventListener('click', removeFromCart)
-}
-
-}
-
+//Copy of array for the products in cart
+let cart = [];
 // pushProductStock()
 //Create a function to update the cart amount value
 function addToCart(e) {
 	let index = e.target.id.replace('articleAddToCart', '');
-	index = Number(index)
+	index = Number(index);
 	productStock[index].Amount += 1;
+	cart = productStock.filter((productStock) => productStock.Amount > 0);
 	pushProductStock();
 	addProductToCart();
 }
-function removeFromCart(e) {
+function subtractFromCart(e) {
 	let index = e.target.id.replace('articleRemoveFromCart', '');
 	if (productStock[index].Amount > 0) {
-	index = Number(index)
-	productStock[index].Amount -= 1;
-	pushProductStock();
-	addProductToCart();
-}
+		index = Number(index);
+		productStock[index].Amount -= 1;
+		pushProductStock();
+		addProductToCart();
+	}
 	// console.log(productStock.Amount)
 }
 
-//Function to test that products are added to cart
+const cartSummary = document.querySelector('.productPreviewSummary');
+const checkoutSummary = document.querySelector('.productCheckoutSummary');
+//Göra om funtkion till forEach, lägg till function i add knappen sen också
 function addProductToCart() {
-    const cartSummary = document.querySelector('.productPreviewSummary');
-    cartSummary.innerHTML = ""; 
+	cartSummary.innerHTML = '';
+	checkoutSummary.innerHTML = '';
+	// checkoutSummary.innerHTML = "";
 
-    for (let i = 0; i < productStock.length; i++) {
-        if (productStock[i].Amount > 0) {
-            cartSummary.innerHTML += `<p>HEJ${productStock[i].name}HEJ</p>`;
-        }
-    }
+	cart.forEach((productStock) => {
+		cartSummary.innerHTML += 
+		//`<p>Product: ${productStock.name}Antal: ${productStock.Amount}<button class="delete" id="delete-${productStock.serialNo}">radera</button></p>`;
+
+			`<li>
+			<img
+				class="productPreviewImage"
+				src="${productStock.image.src}"
+				width="${productStock.image.width}"
+				height="${productStock.image.height}"
+			/>
+			<h3 class="previewProductTitle">${productStock.name}</h3>
+			<p class="previewProductPrice">${productStock.price}</p>
+
+		<div class="productAmountActions">
+			<button class="productAmountSub" >-</button
+			><input class="productAmount" type="number" value="${productStock.Amount}" /><button
+				class="productAmountAdd"
+			>
+				+
+			</button>
+			<button class="previewProductRemoval" id="productRemove${productStock.serialNo}">Remove product
+			</button>
+		</li>`;
+
+
+		checkoutSummary.innerHTML  += `<li class="productAdded">
+		<img
+			class="productCheckoutImage"
+			src="${productStock.image.src}"
+			width="${productStock.image.width}"
+			height="${productStock.image.height}"
+		/>
+
+		<h3 class="ProductCheckoutTitle">${productStock.name}</h3>
+		<p class="ProductCheckoutPrice">${productStock.price}</p>
+		<div class="productAmount">
+			<button class="productAmountSub" >-</button
+			><input class="productAmountValue" type="number" value="${productStock.Amount}"/><button
+				class="productAmountAdd" id="articleAddToCart"
+			>
+				+
+			</button>
+			<button class="previewProductRemoval" id="productRemove${productStock.serialNo}"> Remove product
+								</button>
+		</div>`
+
+	});
+	//Add clickEvent to each of the removal Btns
+	//NOTE FRÅN MUSTAFA, följt från lektionen, men förstår att det är en anonym jäkel, bör vara okej ju med att det en one-liner antar jag?? Annars open för suggestions! 
+	Array.from(document.querySelectorAll('.previewProductRemoval')).forEach((btn) => {
+		btn.addEventListener('click', removeFromCartBtn);
+	});
 }
 
+function removeFromCartBtn(e) {
+	const productSerialNo = Number(e.target.id.replace('productRemove', ''))
 
+	let cartIndex = cart.findIndex(productStock => productStock.serialNo === productSerialNo);
 
+	let productIndex = productStock.findIndex(productStock => productStock.serialNo === productSerialNo);
+
+	if (cartIndex > -1) {
+		
+		cart.splice(cartIndex, 1);
+
+		productStock[productIndex].Amount = 0;
+		pushProductStock();
+		addProductToCart();
+		
+	}
+}
+
+//     for (let i = 0; i < productStock.length; i++) {
+//         if (productStock[i].Amount > 0) {
+//             cartSummary.innerHTML +=
+// 			// `<p>Product: ${productStock[i].name}Antal: ${productStock[i].Amount}</p>`;
+
+// 			`<li>
+// 			<img
+// 				class="productPreviewImage"
+// 				src="${productStock[i].image.src}"
+// 				width="50"
+// 				height="50"
+// 			/>
+// 			<h3 class="previewProductTitle">Test1</h3>
+// 			<p class="previewProductPrice">2590 SEK</p>
+
+// 		<div class="productAmountActions">
+// 			<button class="productAmountSub">-</button
+// 			><input class="productAmount" type="number" value="${productStock[i].Amount}" /><button
+// 				class="productAmountAdd"
+// 			>
+// 				+
+// 			</button>
+// 			<button class="previewProductRemoval">
+// 				<img
+// 					src="Assets/icons/trashcan.svg"
+// 					alt=""
+// 					width="15px"
+// 					height="15px"
+// 				/>
+// 			</button>
+// 		</li>`;
+
+// 		checkoutSummary.innerHTML  += `<li class="productAdded">
+// 		<img
+// 			class="productCheckoutImage"
+// 			src="${productStock[i].image.src}"
+// 			width="${productStock[i].image.width}"
+// 			height="${productStock[i].image.height}"
+// 		/>
+
+// 		<h3 class="ProductCheckoutTitle">${productStock[i].name}</h3>
+// 		<p class="ProductCheckoutPrice">${productStock[i].price}</p>
+// 		<div class="productAmount">
+// 			<button class="productAmountSub">-</button
+// 			><input class="productAmountValue" type="number" value="${productStock[i].Amount}"/><button
+// 				class="productAmountAdd"
+// 			>
+// 				+
+// 			</button>
+// 		</div>
+// 	</li>`
+//         }
+//     }
+// }
+
+//Function to adds products to cart AND summary
+// function addProductToCart() {
+//     const cartSummary = document.querySelector('.productPreviewSummary');  const checkoutSummary = document.querySelector('.productCheckoutSummary');
+
+//     cartSummary.innerHTML = "";
+// 	checkoutSummary.innerHTML = "";
+//     for (let i = 0; i < productStock.length; i++) {
+//         if (productStock[i].Amount > 0) {
+//             cartSummary.innerHTML +=
+// 			// `<p>Product: ${productStock[i].name}Antal: ${productStock[i].Amount}</p>`;
+
+// 			`<li>
+// 			<img
+// 				class="productPreviewImage"
+// 				src="${productStock[i].image.src}"
+// 				width="50"
+// 				height="50"
+// 			/>
+// 			<h3 class="previewProductTitle">Test1</h3>
+// 			<p class="previewProductPrice">2590 SEK</p>
+
+// 		<div class="productAmountActions">
+// 			<button class="productAmountSub">-</button
+// 			><input class="productAmount" type="number" value="${productStock[i].Amount}" /><button
+// 				class="productAmountAdd"
+// 			>
+// 				+
+// 			</button>
+// 			<button class="previewProductRemoval">
+// 				<img
+// 					src="Assets/icons/trashcan.svg"
+// 					alt=""
+// 					width="15px"
+// 					height="15px"
+// 				/>
+// 			</button>
+// 		</li>`;
+
+// 		checkoutSummary.innerHTML  += `<li class="productAdded">
+// 		<img
+// 			class="productCheckoutImage"
+// 			src="${productStock[i].image.src}"
+// 			width="${productStock[i].image.width}"
+// 			height="${productStock[i].image.height}"
+// 		/>
+
+// 		<h3 class="ProductCheckoutTitle">${productStock[i].name}</h3>
+// 		<p class="ProductCheckoutPrice">${productStock[i].price}</p>
+// 		<div class="productAmount">
+// 			<button class="productAmountSub">-</button
+// 			><input class="productAmountValue" type="number" value="${productStock[i].Amount}"/><button
+// 				class="productAmountAdd"
+// 			>
+// 				+
+// 			</button>
+// 		</div>
+// 	</li>`
+//         }
+//     }
+// }
+
+//Function to remove product from cart and Summary by resetting amount to
+// const deleteProduct = document.querySelectorAll('.previewProductRemoval')
+// deleteProduct.addEventListener('click', removeProduct)
+// console.log(deleteProduct)
+// function removeProduct() {
+// 	for (let i = 0; i < productStock.length; i++){
+// 		productStock[i].Amount
+// 	}
+// }
 
 // CHECKOUT PAGE
 // 1.a Remove products:
@@ -433,4 +653,4 @@ function addProductToCart() {
 
 // }
 
-pushProductStock()
+pushProductStock();
